6eec2229b6e05d78e1a64fc0a59a26459f9b2108
Axel Huebl <axel.huebl@web.de>
2014-11-03 11:13:09 +0100

    Add Exit-on-Fail to Build Scripts

--- libs/build.sh
+++ libs/build.sh
@@ -4,26 +4,35 @@
 #
 
 cd vislib
+rm -rf release
 mkdir release
 cd release
 cmake ..
+if [ $? -ne 0 ] ; then exit 1; fi
 make
+if [ $? -ne 0 ] ; then exit 1; fi
 cd ..
 cd ..
 
 cd thelib++
+rm -rf release
 mkdir release
 cd release
 cmake ..
+if [ $? -ne 0 ] ; then exit 1; fi
 make
+if [ $? -ne 0 ] ; then exit 1; fi
 cd ..
 cd ..
 
 cd rivlib
+rm -rf release
 mkdir release
 cd release
 cmake ..
+if [ $? -ne 0 ] ; then exit 1; fi
 make
+if [ $? -ne 0 ] ; then exit 1; fi
 # sudo make install
 cd ..
 cd ..
--- libs/build_all.sh
+++ libs/build_all.sh
@@ -2,43 +2,62 @@
 #
 # Build script
 #
+# Debian/Ubuntu:
+#   apt-get install libxmu-dev libxi-dev freeglut3-dev zlib1g-dev uuid-dev cmake
 
 cd vislib
+rm -rf release
 mkdir release
 cd release
 cmake -DBUILD_TESTS=1 ..
+if [ $? -ne 0 ] ; then exit 1; fi
 make
+if [ $? -ne 0 ] ; then exit 1; fi
 cd ..
+rm -rf debug
 mkdir debug
 cd debug
 cmake -DBUILD_TESTS=1 -DCMAKE_BUILD_TYPE=Debug ..
+if [ $? -ne 0 ] ; then exit 1; fi
 make
+if [ $? -ne 0 ] ; then exit 1; fi
 cd ..
 cd ..
 
 cd thelib++
+rm -rf release
 mkdir release
 cd release
 cmake -DBUILD_TESTS=1 ..
+if [ $? -ne 0 ] ; then exit 1; fi
 make
+if [ $? -ne 0 ] ; then exit 1; fi
 cd ..
+rm -rf debug
 mkdir debug
 cd debug
 cmake -DBUILD_TESTS=1 -DCMAKE_BUILD_TYPE=Debug ..
+if [ $? -ne 0 ] ; then exit 1; fi
 make
+if [ $? -ne 0 ] ; then exit 1; fi
 cd ..
 cd ..
 
 cd rivlib
+rm -rf release
 mkdir release
 cd release
 cmake -DBUILD_TESTS=1 ..
+if [ $? -ne 0 ] ; then exit 1; fi
 make
+if [ $? -ne 0 ] ; then exit 1; fi
 cd ..
+rm -rf debug
 mkdir debug
 cd debug
 cmake -DBUILD_TESTS=1 -DCMAKE_BUILD_TYPE=Debug ..
+if [ $? -ne 0 ] ; then exit 1; fi
 make
+if [ $? -ne 0 ] ; then exit 1; fi
 cd ..
 cd ..
-
